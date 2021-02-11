3a5456b1be5a4131eb404568f104ec050d0ebe74
Eyhenij <Eyhenij@gmail.com>
2021-02-11 12:56:40 +0300

    11.02.2021-001 feat: use angular.material in sidebar.component and edit-user-page.component, use getUserById method in edit-user-page.component to get data by route params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
+import {UsersService} from './users.service';
+import {HttpClientModule} from '@angular/common/http';
+
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {ContentComponent} from './content/content.component';
 import {OnLoadingComponent} from './on-loading/on-loading.component';
-import {UsersService} from './users.service';
-import {HttpClientModule} from '@angular/common/http';
-import {NewUserPageComponent} from './pages/new-user-page/new-user-page.component';
 import {UserPageComponent} from './pages/user-page/user-page.component';
+import {EditUserPageComponent} from './pages/edit-user-page/edit-user-page.component';
+import {NewUserPageComponent} from './pages/new-user-page/new-user-page.component';
 import {HomePageComponent} from './pages/home-page/home-page.component';
-import {UnknownDataPageComponent} from './pages/unknown-data-page/unknown-data-page.component';
 import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
-import { EditUserPageComponent } from './pages/edit-user-page/edit-user-page.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatOptionModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
     declarations: [
@@ -26,14 +33,19 @@ import { EditUserPageComponent } from './pages/edit-user-page/edit-user-page.com
         NewUserPageComponent,
         UserPageComponent,
         HomePageComponent,
-        UnknownDataPageComponent,
         NotFoundPageComponent,
         EditUserPageComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonToggleModule,
+        MatSliderModule,
+        MatButtonModule,
+        MatOptionModule,
+        MatCardModule
     ],
     providers: [UsersService],
     bootstrap: [AppComponent]
